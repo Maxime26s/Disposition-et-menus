2286c5ede738d4c039344664b919adec9561c26c
Maxime <wixxwistiii@gmail.com>
2018-10-04 09:03:59 -0400

    boo!2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -22,24 +25,27 @@ public class Main extends Application {
         BorderPane borderPane = new BorderPane();
         Scene scene = new Scene(borderPane);
 
-        primaryStage.setWidth(400);
-        primaryStage.setHeight(600);
+        primaryStage.setWidth(1000);
+        primaryStage.setHeight(1000);
         primaryStage.setTitle("Laboratoire 6");
         primaryStage.setResizable(false);
 
-        creerMenu(borderPane);
-        image0 = new Image("file:chat.jpg");
-        image1 = new Image("file:chat.jpg");
-        image2 = new Image("file:chat.jpg");
-        image3 = new Image("file:chat.jpg");
+        image0 = new Image("file:image0.jpg");
+        image1 = new Image("file:image1.jpg");
+        image2 = new Image("file:image2.jpg");
+        image3 = new Image("file:image3.jpg");
         imageView = new ImageView(image0);
+        imageView.setFitHeight(500);
+        imageView.setFitWidth(500);
+
+        creerSlider(borderPane,creerMenu(borderPane));
 
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    private void creerMenu(BorderPane borderPane){
-        Menu menuFichier = new Menu("Fichier");
+    private MenuItem creerMenu(BorderPane borderPane){
+        Menu menuFichier = new Menu("Fichiers");
         Menu menuChanger = new Menu("Changer une image");
         MenuItem itemImage1 = new MenuItem("Image #1");
         MenuItem itemImage2 = new MenuItem("Image #2");
@@ -52,37 +58,42 @@ public class Main extends Application {
         ContextMenu contextMenu = new ContextMenu(menuFichier, menuAction);
         MenuBar menuBar = new MenuBar(menuFichier, menuAction);
         borderPane.setTop(menuBar);
-        borderPane.getChildren().add(borderPane);
         imageView.setOnContextMenuRequested(event -> contextMenu.show(imageView, event.getScreenX(), event.getScreenY()));
 
         itemImage1.setOnAction(event -> imageView.setImage(image1));
         itemImage2.setOnAction(event -> imageView.setImage(image2));
         itemImage3.setOnAction(event -> imageView.setImage(image3));
+
+        return itemReinitialiser;
     }
 
-    private void creerSlider(BorderPane borderPane){
+    private void creerSlider(BorderPane borderPane, MenuItem menuItem){
         ColorAdjust imageColors = new ColorAdjust();
         imageView.setEffect(imageColors);
 
-        Label labelLuminosite = new Label("Volume:");
+        Label labelLuminosite = new Label("Luminosité:");
         Slider sliderLuminosite = new Slider(-1, 1, 0);
         sliderLuminosite.valueProperty().addListener((observable, oldValue, newValue) -> imageColors.setBrightness((double)newValue));
-        borderPane.getChildren().add(labelLuminosite);
-        borderPane.getChildren().add(sliderLuminosite);
-        Label labelContraste = new Label("Volume:");
+        Label labelContraste = new Label("Contraste:");
         Slider sliderContraste = new Slider(-1, 1, 0);
-        borderPane.getChildren().add(labelContraste);
-        borderPane.getChildren().add(sliderContraste);
         sliderContraste.valueProperty().addListener((observable, oldValue, newValue) -> imageColors.setContrast((double)newValue));
-        Label labelTeinte = new Label("Volume:");
+        Label labelTeinte = new Label("Teinte:");
         Slider sliderTeinte = new Slider(-1, 1, 0);
         sliderTeinte.valueProperty().addListener((observable, oldValue, newValue) -> imageColors.setHue((double)newValue));
-        borderPane.getChildren().add(labelTeinte);
-        borderPane.getChildren().add(sliderTeinte);
-        Label labelSaturation = new Label("Volume:");
+        Label labelSaturation = new Label("Saturation:");
         Slider sliderSaturation = new Slider(-1, 1, 0);
         sliderSaturation.valueProperty().addListener((observable, oldValue, newValue) -> imageColors.setSaturation((double)newValue));
-        borderPane.getChildren().add(labelSaturation);
-        borderPane.getChildren().add(sliderSaturation);
+        VBox vBox = new VBox(labelLuminosite,sliderLuminosite,labelContraste,sliderContraste,labelTeinte,sliderTeinte,labelSaturation,sliderSaturation);
+        vBox.setAlignment(Pos.CENTER);
+        HBox hBox = new HBox(imageView,vBox);
+        hBox.setAlignment(Pos.CENTER);
+        borderPane.setCenter(hBox);
+
+        menuItem.setOnAction(event -> {
+            sliderLuminosite.setValue(0);
+            sliderContraste.setValue(0);
+            sliderTeinte.setValue(0);
+            sliderSaturation.setValue(0);
+        });
     }
 }
