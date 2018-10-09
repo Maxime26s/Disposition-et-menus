b2f2e6160037ee471e0ae01d0dbb51959ed713ae
Maxime <wixxwistiii@gmail.com>
2018-10-09 03:09:05 -0400

    yeeeeeee

--- src/Main.java
+++ src/Main.java
@@ -25,8 +25,7 @@ public class Main extends Application {
         BorderPane borderPane = new BorderPane();
         Scene scene = new Scene(borderPane);
 
-        primaryStage.setWidth(1000);
-        primaryStage.setHeight(1000);
+        primaryStage.setMaximized(true);
         primaryStage.setTitle("Laboratoire 6");
         primaryStage.setResizable(false);
 
@@ -35,16 +34,16 @@ public class Main extends Application {
         image2 = new Image("file:image2.jpg");
         image3 = new Image("file:image3.jpg");
         imageView = new ImageView(image0);
-        imageView.setFitHeight(500);
         imageView.setFitWidth(500);
+        imageView.setPreserveRatio(true);
 
-        creerSlider(borderPane,creerMenu(borderPane));
+        creerSlider(borderPane,creerMenuBar(borderPane),creerMenuContext(borderPane));
 
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    private MenuItem creerMenu(BorderPane borderPane){
+    private MenuItem creerMenuContext(BorderPane borderPane){
         Menu menuFichier = new Menu("Fichiers");
         Menu menuChanger = new Menu("Changer une image");
         MenuItem itemImage1 = new MenuItem("Image #1");
@@ -56,8 +55,6 @@ public class Main extends Application {
         MenuItem itemReinitialiser = new MenuItem("Réinitialiser");
         menuAction.getItems().addAll(itemReinitialiser);
         ContextMenu contextMenu = new ContextMenu(menuFichier, menuAction);
-        MenuBar menuBar = new MenuBar(menuFichier, menuAction);
-        borderPane.setTop(menuBar);
         imageView.setOnContextMenuRequested(event -> contextMenu.show(imageView, event.getScreenX(), event.getScreenY()));
 
         itemImage1.setOnAction(event -> imageView.setImage(image1));
@@ -67,29 +64,65 @@ public class Main extends Application {
         return itemReinitialiser;
     }
 
-    private void creerSlider(BorderPane borderPane, MenuItem menuItem){
+    private MenuItem creerMenuBar(BorderPane borderPane){
+        Menu menuFichier = new Menu("Fichiers");
+        Menu menuChanger = new Menu("Changer une image");
+        MenuItem itemImage1 = new MenuItem("Image #1");
+        MenuItem itemImage2 = new MenuItem("Image #2");
+        MenuItem itemImage3 = new MenuItem("Image #3");
+        menuFichier.getItems().addAll(menuChanger);
+        menuChanger.getItems().addAll(itemImage1, itemImage2, itemImage3);
+        Menu menuAction = new Menu("Actions");
+        MenuItem itemReinitialiser = new MenuItem("Réinitialiser");
+        menuAction.getItems().addAll(itemReinitialiser);
+        MenuBar menuBar = new MenuBar(menuFichier, menuAction);
+        borderPane.setTop(menuBar);
+
+        itemImage1.setOnAction(event -> imageView.setImage(image1));
+        itemImage2.setOnAction(event -> imageView.setImage(image2));
+        itemImage3.setOnAction(event -> imageView.setImage(image3));
+
+        return itemReinitialiser;
+    }
+
+    private void creerSlider(BorderPane borderPane, MenuItem menuItemBar,MenuItem menuItemContext){
         ColorAdjust imageColors = new ColorAdjust();
         imageView.setEffect(imageColors);
 
         Label labelLuminosite = new Label("Luminosité:");
         Slider sliderLuminosite = new Slider(-1, 1, 0);
         sliderLuminosite.valueProperty().addListener((observable, oldValue, newValue) -> imageColors.setBrightness((double)newValue));
+        Tooltip tooltipLuminosite = new Tooltip("Rend l'image plus claire ou plus sombre");
+        sliderLuminosite.setTooltip(tooltipLuminosite);
         Label labelContraste = new Label("Contraste:");
         Slider sliderContraste = new Slider(-1, 1, 0);
         sliderContraste.valueProperty().addListener((observable, oldValue, newValue) -> imageColors.setContrast((double)newValue));
+        Tooltip tooltipContraste = new Tooltip("Diminue ou augmente la différence entre les couleurs");
+        sliderContraste.setTooltip(tooltipContraste);
         Label labelTeinte = new Label("Teinte:");
         Slider sliderTeinte = new Slider(-1, 1, 0);
         sliderTeinte.valueProperty().addListener((observable, oldValue, newValue) -> imageColors.setHue((double)newValue));
+        Tooltip tooltipTeinte = new Tooltip("Change la teinte (couleur) de l'image");
+        sliderTeinte.setTooltip(tooltipTeinte);
         Label labelSaturation = new Label("Saturation:");
         Slider sliderSaturation = new Slider(-1, 1, 0);
         sliderSaturation.valueProperty().addListener((observable, oldValue, newValue) -> imageColors.setSaturation((double)newValue));
+        Tooltip tooltipSaturation = new Tooltip("Diminue ou augmente l'intensité des couleurs");
+        sliderSaturation.setTooltip(tooltipSaturation);
         VBox vBox = new VBox(labelLuminosite,sliderLuminosite,labelContraste,sliderContraste,labelTeinte,sliderTeinte,labelSaturation,sliderSaturation);
         vBox.setAlignment(Pos.CENTER);
         HBox hBox = new HBox(imageView,vBox);
         hBox.setAlignment(Pos.CENTER);
         borderPane.setCenter(hBox);
 
-        menuItem.setOnAction(event -> {
+        menuItemBar.setOnAction(event -> {
+            sliderLuminosite.setValue(0);
+            sliderContraste.setValue(0);
+            sliderTeinte.setValue(0);
+            sliderSaturation.setValue(0);
+        });
+
+        menuItemContext.setOnAction(event -> {
             sliderLuminosite.setValue(0);
             sliderContraste.setValue(0);
             sliderTeinte.setValue(0);
